6785be0c6246a1dd0370b8390742192268fc6b0f
a5 <a5@LS3-PC28>
2017-02-13 13:33:01 -0600

    avance contaco

---
+++ src/ep3_i2017_paholasanchez_allanmartinez/Contacto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ep3_i2017_paholasanchez_allanmartinez;
+
+/**
+ *
+ * @author a5
+ */
+public class Contacto {
+    public int n;
+}
