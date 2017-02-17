14dd857fe34199390a5efdf9065924310435217a
axel <axel@axel-Ing>
2017-02-17 03:11:53 -0600

    FaltaPoco

--- src/ep3_i2017_paholasanchez_allanmartinez/Agenda.java
+++ src/ep3_i2017_paholasanchez_allanmartinez/Agenda.java
@@ -15,8 +15,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.TreeSet;
 
 /**
  *
@@ -64,8 +63,6 @@ public class Agenda {
         throw new ExcepcionGrupo();
     }
 
-   
-
     public void insertarContactos(Grupo grupo) {
         Contacto contacto;
         Scanner teclado = new Scanner(System.in);
@@ -94,7 +91,7 @@ public class Agenda {
         if (agenda.get(grupo).isEmpty()) {
             return 0;
         } else {
-            for (int i = 0; i < agenda.size(); i++) {
+            for (int i = 0; i < agenda.get(grupo).size(); i++) {
                 if (nuevo.compareTo(agenda.get(grupo).get(i)) > 0 || nuevo.compareTo(agenda.get(grupo).get(i)) == 0) {
                     return i;
                 }
@@ -170,7 +167,7 @@ public class Agenda {
                         do {
                             contAux = new Contacto();
                             contAux.ingresarDatos();
-                            agenda.get(grAux).add(ordenar(grAux,contAux),contAux);
+                            agenda.get(grAux).add(ordenar(grAux, contAux), contAux);
                             //agenda.get(stAux).add(insertarContactos());
                             System.out.println("Deseas añadir otro contacto presiona 1[SI] [0]NO");
                         } while (teclado.nextInt() != 0);
@@ -220,11 +217,12 @@ public class Agenda {
                 break;
 
             case 5:
-
-            //Consultar todas las tarjetas de contacto diferentes
+                //Consultar todas las tarjetas de contacto diferentes
+                encontrarTodosContactos();
                 break;
 
             case 6://Consultar contactos que pertenezcan a más de un grupo
+                encontrarContVariosGrup();
                 break;
 
             case 7://Consultar un grupo
@@ -254,6 +252,71 @@ public class Agenda {
                 System.out.println("Opcion Invalida");
         }
         return opc;
+    }
+
+    public void encontrarTodosContactos() {
+        TreeSet<Contacto> arbol = new TreeSet<Contacto>();
+        Iterator i = agenda.keySet().iterator();
+        Iterator j;
+        Grupo grupoAux;
+        Contacto auxCont;
+        while (i.hasNext()) {
+            grupoAux = (Grupo) i.next();
+            arbol.addAll(agenda.get(grupoAux).subList(0, agenda.get(grupoAux).size()));
+
+        }
+        System.out.println(arbol.size());
+        imprimirContactosEspecial(arbol, "sin repetir encontrados: ");
+    }
+
+    public void imprimirContactosEspecial(TreeSet<Contacto> arbol, String enunciado) {
+        System.out.println("Los contactos " + enunciado + " :\n");
+        Iterator<Contacto> i = arbol.iterator();
+        Contacto aux;
+        while (i.hasNext()) {
+            aux = i.next();
+            aux.mostrarDatos();
+        }
+    }
+
+    public void encontrarContVariosGrup() {
+        TreeSet<Contacto> arbol = new TreeSet<Contacto>();
+        Iterator i = agenda.keySet().iterator();
+        Iterator<Contacto> j;
+        Grupo grupoAux;
+        Contacto auxCont;
+        while (i.hasNext()) {
+            grupoAux = (Grupo) i.next();
+            j=agenda.get(grupoAux).iterator();
+            while (j.hasNext()) {
+                auxCont=j.next();
+                if(compararContacto(auxCont)!=null){
+                    arbol.add(auxCont);
+                }
+            }
+        }
+
+        imprimirContactosEspecial(arbol, "encontrados en varios grupos ");
+    }
+    public Contacto compararContacto(Contacto repetido){
+        Iterator<Grupo> i=agenda.keySet().iterator();
+        Grupo aux;
+        int cont=0;
+         System.out.println("s");
+        while(i.hasNext()){
+               System.out.println("e");     
+            aux=i.next();
+            if(agenda.get(aux).contains(repetido)){
+                repetido.mostrarDatos();
+                   
+                cont++;
+            }
+            
+        }
+        if(cont>1){
+                return repetido;
+            }
+        return null;
     }
 
     public void mostrarTodo() {
